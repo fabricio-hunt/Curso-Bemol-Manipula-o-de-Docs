eadf232e526dbe037333de1aecd4c5b439c74e75
Fabricio Macedo <fabriciobaraunadev@gmail.com>
2025-06-27 09:45:08 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+nomes = [
+    "Alice",
+    "Bob",
+    "Charlie",
+    "David",
+    "Eve",
+    "Frank",
+    "Grace",
+    "Henry",
+    "Ivy",
+    "Jack",
+    "Kate",
+    "Liam",
+    "Mia",]
+
+
+for nome in nomes:
+    print(nome.upper())
